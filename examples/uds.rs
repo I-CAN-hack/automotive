033c8dd4d36903ac936255b52fe231f88c75ca0f
Willem Melching <willem.melching@gmail.com>
2024-02-24 17:51:46 +0100

    clean up uds example

--- examples/uds.rs
+++ examples/uds.rs
@@ -1,21 +1,22 @@
-use automotive::uds::constants::DataIdentifier;
+use automotive::uds::constants::{DataIdentifier, SessionType};
+use bstr::ByteSlice;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
 
-    let adapter = automotive::adapter::get_adapter().unwrap();
+    let adapter = automotive::adapter::get_adapter()?;
     let isotp = automotive::isotp::IsoTPAdapter::from_id(&adapter, 0x7a1);
     let uds = automotive::uds::UDSClient::new(&isotp);
 
-    uds.tester_present().await.unwrap();
-    let response = uds
-        .read_data_by_identifier(DataIdentifier::ApplicationSoftwareIdentification as u16)
-        .await
-        .unwrap();
+    uds.tester_present().await?;
+    uds.diagnostic_session_control(SessionType::ExtendedDiagnostic as u8).await?;
 
-    println!(
-        "Application Software Identification: {}",
-        hex::encode(response)
-    );
+    let did = DataIdentifier::ApplicationSoftwareIdentification;
+    let resp = uds.read_data_by_identifier(did as u16).await?;
+
+    // ApplicationSoftwareIdentification: "\x018965B4209000\0\0\0\0"
+    println!("{:?}: {:?}", did, resp.as_bstr());
+
+    Ok(())
 }
