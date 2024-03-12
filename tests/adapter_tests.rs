9a083ddcd0a9981d1e3ce949f1126dce06bb6a3d
Willem Melching <willem.melching@gmail.com>
2024-03-12 11:44:34 +0100

    add panda tests

---
+++ tests/adapter_tests.rs
@@ -0,0 +1,67 @@
+#![allow(dead_code, unused_imports)]
+use std::time::Duration;
+use automotive::async_can::AsyncCanAdapter;
+use automotive::can::{CanAdapter, Frame};
+use automotive::panda::Panda;
+
+static BULK_NUM_FRAMES: u64 = 0x400;
+static BULK_TIMEOUT_MS: u64 = 1000;
+
+/// Sends a large number of frames to a "blocking" adapter, and then reads back all sent messages.
+/// This verified the adapter doesn't drop messages and reads them back in the same order as they are sent,
+/// which is needed for the async adapter to work correctly.
+fn bulk_send_sync<T: CanAdapter>(adapter: &mut T) {
+    let mut frames = vec![];
+
+    for i in 0..BULK_NUM_FRAMES {
+        frames.push(Frame::new(0, 0x123.into(), &i.to_be_bytes()));
+    }
+
+    adapter.send(&frames).unwrap();
+
+    let start = std::time::Instant::now();
+
+    let mut received: Vec<Frame> = vec![];
+    while received.len() < frames.len() && start.elapsed() < Duration::from_millis(BULK_TIMEOUT_MS) {
+        let rx = adapter.recv().unwrap();
+        let rx: Vec<Frame> = rx.into_iter().filter(|frame| frame.returned).collect();
+
+        for frame in rx {
+            let mut copy = frame.clone();
+            copy.returned = false;
+            received.push(copy);
+        }
+    }
+
+    assert_eq!(frames, received);
+}
+
+/// Sends a large number of frames to the adapter, and awaits them simultaneously.
+/// This tests the functionality in [`AsyncCanAdapter`] to resolve the future when the message is ACKed.
+async fn bulk_send(adapter: &AsyncCanAdapter) {
+    let mut frames = vec![];
+
+    for i in 0..BULK_NUM_FRAMES {
+        frames.push(Frame::new(0, 0x123.into(), &i.to_be_bytes()));
+    }
+
+    let r = frames.iter().map(|frame| adapter.send(frame));
+    tokio::time::timeout(Duration::from_millis(BULK_TIMEOUT_MS), futures::future::join_all(r)).await.unwrap();
+}
+
+#[cfg(feature = "test_panda")]
+#[test]
+#[serial_test::serial]
+fn panda_bulk_send_sync() {
+    let mut panda = Panda::new().unwrap();
+    bulk_send_sync(&mut panda);
+}
+
+
+#[cfg(feature = "test_panda")]
+#[tokio::test]
+#[serial_test::serial]
+async fn panda_bulk_send_async() {
+    let panda = automotive::panda::Panda::new_async().unwrap();
+    bulk_send(&panda).await;
+}
