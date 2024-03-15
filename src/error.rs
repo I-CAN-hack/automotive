711a79b685d9ef1960062509ce340e78ec7cc79c
Willem Melching <willem.melching@gmail.com>
2024-03-15 14:34:21 +0100

    clean up Error definitions using thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,42 +1,28 @@
 //! Contains the main error type for the library.
-use std::fmt;
+use thiserror::Error;
 
 /// The main error type for the library. Each module has it's own error type that is contained by this error.
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[derive(Error, Debug, PartialEq)]
 pub enum Error {
+    #[error("Not Found")]
     NotFound,
+    #[error("Malformed Frame")]
     MalformedFrame,
+    #[error("Timeout")]
     Timeout,
+    #[error(transparent)]
     IsoTPError(crate::isotp::error::Error),
-    LibUsbError(rusb::Error),
+    #[error(transparent)]
+    LibUsbError(#[from] rusb::Error),
+    #[error(transparent)]
     PandaError(crate::panda::error::Error),
+    #[error(transparent)]
     UDSError(crate::uds::error::Error),
 }
 
-impl From<rusb::Error> for Error {
-    fn from(err: rusb::Error) -> Error {
-        Error::LibUsbError(err)
-    }
-}
 
 impl From<tokio_stream::Elapsed> for Error {
     fn from(_: tokio_stream::Elapsed) -> Error {
         Error::Timeout
     }
 }
-
-impl fmt::Display for Error {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Error::NotFound => write!(fmt, "Not Found"),
-            Error::MalformedFrame => write!(fmt, "Malformed Frame"),
-            Error::Timeout => write!(fmt, "Timeout"),
-            Error::IsoTPError(err) => err.fmt(fmt),
-            Error::LibUsbError(err) => err.fmt(fmt),
-            Error::PandaError(err) => err.fmt(fmt),
-            Error::UDSError(err) => err.fmt(fmt),
-        }
-    }
-}
-
-impl std::error::Error for Error {}
--- src/uds/error.rs
+++ src/uds/error.rs
@@ -1,5 +1,5 @@
 //! Error types for the UDS Client.
-use std::fmt;
+use thiserror::Error;
 
 /// Negative Response Codes returned by ECU as defined in ISO 14229
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
@@ -61,36 +61,20 @@ impl From<u8> for NegativeResponseCode {
     }
 }
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[derive(Error, Debug, PartialEq)]
 pub enum Error {
+    #[error("Invalid Reponse Service ID: {0}")]
     InvalidServiceId(u8),
+    #[error("Invalid Response Sub Function ID: {0}")]
     InvalidSubFunction(u8),
+    #[error("Invalid Response Data Identifer: {0}")]
     InvalidDataIdentifier(u16),
+    #[error("Invalid Response Routine Identifer: {0}")]
     InvalidRoutineIdentifier(u16),
+    #[error("Invalid Block Sequence Counter: {0}")]
     InvalidBlockSequenceCounter(u8),
+    #[error("Invalid Response Length")]
     InvalidResponseLength,
+    #[error("Negative Response: {0:?}")]
     NegativeResponse(NegativeResponseCode),
 }
-
-impl fmt::Display for Error {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Error::InvalidServiceId(id) => write!(fmt, "Invalid Response Service ID: {}", id),
-            Error::InvalidSubFunction(id) => {
-                write!(fmt, "Invalid Response Sub Function ID: {}", id)
-            }
-            Error::InvalidDataIdentifier(id) => {
-                write!(fmt, "Invalid Response Data Identifer: {}", id)
-            }
-            Error::InvalidRoutineIdentifier(id) => {
-                write!(fmt, "Invalid Response Routine Identifer: {}", id)
-            }
-            Error::InvalidBlockSequenceCounter(id) => {
-                write!(fmt, "Invalid Block Sequence Counter: {}", id)
-            }
-            Error::InvalidResponseLength => write!(fmt, "Invalid Response Length"),
-            Error::NegativeResponse(e) => write!(fmt, "Negative Response: {:?}", e),
-        }
-    }
-}
-impl std::error::Error for Error {}
