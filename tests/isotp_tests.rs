44289c4e5ec6a47418119b23b4060b11315003a2
Willem Melching <willem.melching@gmail.com>
2024-03-18 14:17:12 +0100

    ensure vecu is killed on test failure

--- tests/isotp_tests.rs
+++ tests/isotp_tests.rs
@@ -3,16 +3,23 @@ use automotive::async_can::AsyncCanAdapter;
 use automotive::can::Identifier;
 use automotive::isotp::{IsoTPAdapter, IsoTPConfig};
 use tokio_stream::StreamExt;
-use std::process::Command;
+use std::process::{Command, Child};
 use std::vec;
 
 static VECU_STARTUP_TIMEOUT_MS: u64 = 1000;
 
-async fn vecu_spawn(adapter: &AsyncCanAdapter) -> std::process::Child {
+struct ChildGuard(Child);
+impl Drop for ChildGuard {
+    fn drop(&mut self) {
+        self.0.kill().unwrap()
+    }
+}
+
+async fn vecu_spawn(adapter: &AsyncCanAdapter) -> ChildGuard {
     let stream = adapter.recv().timeout(std::time::Duration::from_millis(VECU_STARTUP_TIMEOUT_MS));
     tokio::pin!(stream);
 
-    let vecu = Command::new("scripts/vecu.py").spawn().unwrap();
+    let vecu = ChildGuard(Command::new("scripts/vecu.py").spawn().unwrap());
     stream.next().await.unwrap().expect("vecu did not start");
 
     vecu
@@ -21,7 +28,7 @@ async fn vecu_spawn(adapter: &AsyncCanAdapter) -> std::process::Child {
 
 async fn isotp_echo(msg_len: usize) {
     let adapter = automotive::socketcan::SocketCan::new_async_from_name("vcan0").unwrap();
-    let mut vecu = vecu_spawn(&adapter).await;
+    let _vecu = vecu_spawn(&adapter).await;
 
     let config = IsoTPConfig::new(0, Identifier::Standard(0x7a1));
     let isotp = IsoTPAdapter::new(&adapter, config);
@@ -32,9 +39,6 @@ async fn isotp_echo(msg_len: usize) {
     let response = stream.next().await.unwrap().unwrap();
 
     assert_eq!(response, request);
-
-    vecu.kill().unwrap();
-
 }
 
 #[cfg(feature = "test_vcan")]
@@ -49,4 +53,11 @@ async fn isotp_test_single_frame() {
 #[serial_test::serial]
 async fn isotp_test_flow_control() {
     isotp_echo(64).await;
+}
+
+#[cfg(feature = "test_vcan")]
+#[tokio::test]
+#[serial_test::serial]
+async fn isotp_test_cf_idx_overflow() {
+    isotp_echo(256).await;
 }
