e521023236111a58e0375593ada1de934faeb1ef
Willem Melching <willem.melching@gmail.com>
2024-03-19 18:58:55 +0100

    cleanup

--- scripts/build_can_isotp.sh
+++ scripts/build_can_isotp.sh
@@ -6,6 +6,6 @@ git clone https://github.com/hartkopp/can-isotp.git
 cd can-isotp
 git checkout mainline-5.4+
 make
-# sudo make modules_install
+
 sudo modprobe can
 sudo insmod ./net/can/can-isotp.ko
