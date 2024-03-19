54cad0735e9398ab4c792980731b0ad0262f39fd
Willem Melching <willem.melching@gmail.com>
2024-03-19 18:26:23 +0100

    use branch

--- scripts/build_can_isotp.sh
+++ scripts/build_can_isotp.sh
@@ -4,5 +4,6 @@ set -e
 cd /tmp
 git clone https://github.com/hartkopp/can-isotp.git
 cd can-isotp
+git checkout mainline-5.4+
 make
-sudo make modules_install
+sudo insmod ./net/can/can-isotp.ko
--- scripts/set_up_vcan.sh
+++ scripts/set_up_vcan.sh
@@ -2,7 +2,6 @@
 set -e
 
 sudo modprobe can
-sudo modprobe can-isotp
 sudo modprobe vcan
 sudo ip link add dev vcan0 type vcan
 sudo ip link set dev vcan0 up
