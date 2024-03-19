749f9ea4bad4bb17866b90668d166ad8131bc46d
Willem Melching <willem.melching@gmail.com>
2024-03-19 17:00:31 +0100

    implement extended addressing

--- tests/isotp_tests.rs
+++ tests/isotp_tests.rs
@@ -20,6 +20,7 @@ struct VECUConfig {
     pub bs: u32,
     pub padding: Option<u8>,
     pub fd: bool,
+    pub ext_address: Option<u8>,
 }
 
 impl VECUConfig {
@@ -35,6 +36,11 @@ impl VECUConfig {
         if let Some(padding) = self.padding {
             result.push("--padding".to_owned());
             result.push(format!("{}", padding));
+        }
+
+        if let Some(ext_address) = self.ext_address {
+            result.push("--ext-address".to_owned());
+            result.push(format!("{}", ext_address));
         }
 
         if self.fd {
@@ -69,6 +75,7 @@ async fn isotp_test_echo(msg_len: usize, config: VECUConfig) {
     let mut isotp_config = IsoTPConfig::new(0, Identifier::Standard(0x7a1));
     isotp_config.padding = config.padding;
     isotp_config.fd = config.fd;
+    isotp_config.ext_address = config.ext_address;
 
     let isotp = IsoTPAdapter::new(&adapter, isotp_config);
 
@@ -149,6 +156,47 @@ async fn isotp_test_bs() {
 async fn isotp_test_fd() {
     let config = VECUConfig {
         fd: true,
+        ..Default::default()
+    };
+
+    // Single frame escape
+    isotp_test_echo(62, config).await;
+
+    // Single frame with some padding to reach next DLC
+    isotp_test_echo(50, config).await;
+
+    // Multiple frames
+    isotp_test_echo(256, config).await;
+
+    // First frame escape
+    isotp_test_echo(5000, config).await;
+}
+
+#[cfg(feature = "test_vcan")]
+#[tokio::test]
+#[serial_test::serial]
+async fn isotp_test_extended() {
+    let config = VECUConfig {
+        ext_address: Some(0xff),
+        ..Default::default()
+    };
+    // Single frame
+    isotp_test_echo(1, config).await;
+    isotp_test_echo(7, config).await;
+    // Flow control
+    isotp_test_echo(62, config).await; // No padding on last CF
+    isotp_test_echo(64, config).await;
+    // Overflow IDX in flow control
+    isotp_test_echo(256, config).await;
+}
+
+#[cfg(feature = "test_vcan")]
+#[tokio::test]
+#[serial_test::serial]
+async fn isotp_test_fd_extended() {
+    let config = VECUConfig {
+        fd: true,
+        ext_address: Some(0xff),
         ..Default::default()
     };
 
