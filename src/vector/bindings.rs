f6154e8e3cc8c962d750601272089e856ca0a657
Willem Melching <willem.melching@gmail.com>
2024-11-26 01:03:42 -0800

    Allow missing bindings file

---
+++ src/vector/bindings.rs
@@ -0,0 +1,7 @@
+#![allow(clippy::all)]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+#![allow(unused_imports)]
+include!(concat!(env!("OUT_DIR"), "/vxlapi_bindings.rs"));
