9b84063c0baa7c97a44a555cc1cc4bd163471d11
Willem Melching <willem.melching@gmail.com>
2024-12-06 06:39:47 -0800

    Fix warning

--- src/vector/vxlapi.rs
+++ src/vector/vxlapi.rs
@@ -103,8 +103,7 @@ pub fn xl_get_channel_mask(app_config: &ChannelConfig) -> Result<XLaccess> {
 pub fn xl_open_port(user_name: &str, access_mask: XLaccess) -> Result<PortHandle> {
     unsafe {
         let mut port_handle = std::mem::zeroed();
-        let mut permission_mask = std::mem::zeroed();
-        permission_mask = access_mask; // Request init access
+        let mut permission_mask = access_mask; // Request init access so we can change bitrate
 
         let status = xl::xlOpenPort(
             &mut port_handle,
