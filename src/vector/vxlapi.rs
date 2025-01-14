e2f76e5192f69286e8a5a8ec16a1a0decda1648e
Willem Melching <willem.melching@gmail.com>
2025-01-14 14:08:55 +0100

    fix typo

--- src/vector/vxlapi.rs
+++ src/vector/vxlapi.rs
@@ -105,7 +105,7 @@ pub fn xl_open_port(user_name: &str, access_mask: XLaccess, init: bool) -> Resul
         let mut port_handle = std::mem::zeroed();
         let mut permission_mask = std::mem::zeroed();
         if init {
-            access_mask = access_mask; // Request init access so we can change bitrate
+            permission_mask = access_mask; // Request init access so we can change bitrate
         }
 
         let status = xl::xlOpenPort(
