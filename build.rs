512efe63102c7f0276b28c63b11ad905b125329e
Willem Melching <willem.melching@gmail.com>
2026-02-11 12:19:42 +0100

    Fix native windows build

--- build.rs
+++ build.rs
@@ -64,11 +64,7 @@ fn main() {
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
     let vector_xl_enabled = env::var_os("CARGO_FEATURE_VECTOR_XL").is_some();
 
-    let host = env::var("HOST").unwrap_or_default();
-    let target = env::var("TARGET").unwrap_or_default();
-    let is_cross = host != target;
-
-    if target_os == "windows" && vector_xl_enabled && is_cross {
+    if target_os == "windows" && vector_xl_enabled {
         #[cfg(feature = "vector-xl")]
         build_vxlapi();
     }
