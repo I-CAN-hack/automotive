a65cd096bb75d9e4487518b0adda42151179bd42
Willem Melching <willem.melching@gmail.com>
2026-03-13 10:19:51 +0100

    Improve documentation for BitrateBuilder (#102)
    * add example
    
    * add to docs
    
    * add missing trait

---
+++ examples/configure_bitrate.rs
@@ -0,0 +1,36 @@
+#[cfg(all(target_os = "windows", feature = "vector-xl"))]
+pub async fn run() -> automotive::Result<()> {
+    use automotive::can::bitrate::BitrateBuilder;
+    use automotive::vector::VectorCan;
+    use automotive::StreamExt;
+
+    tracing_subscriber::fmt::init();
+
+    let bitrate_cfg = BitrateBuilder::new::<VectorCan>()
+        .bitrate(500_000)
+        .sample_point(0.8)
+        .data_bitrate(2_000_000)
+        .data_sample_point(0.8)
+        .build()
+        .unwrap();
+
+    let adapter = VectorCan::new_async(0, &Some(bitrate_cfg.into()))?;
+    let mut stream = adapter.recv();
+
+    while let Some(frame) = stream.next().await {
+        println!("{:?}", frame);
+    }
+
+    Ok(())
+}
+
+#[cfg(all(target_os = "windows", feature = "vector-xl"))]
+#[tokio::main]
+async fn main() -> automotive::Result<()> {
+    run().await
+}
+
+#[cfg(not(all(target_os = "windows", feature = "vector-xl")))]
+fn main() {
+    eprintln!("This example requires Windows and the `vector-xl` feature.");
+}
