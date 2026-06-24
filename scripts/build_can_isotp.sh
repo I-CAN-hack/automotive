d17b7720cfa1d51172a266a54695d07cd540c5bc
Willem Melching <willem.melching@gmail.com>
2026-06-24 14:51:10 +0200

    ci: fix isotp kernel module build (#111)

--- scripts/build_can_isotp.sh
+++ scripts/build_can_isotp.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env sh
 set -e
 
-cd /tmp
-git clone https://github.com/hartkopp/can-isotp.git
-cd can-isotp
-git checkout mainline-5.4+
+sudo modprobe can
+
+if sudo modprobe can-isotp 2>/dev/null; then
+  exit 0
+fi
+
+tmpdir="$(mktemp -d)"
+trap 'rm -rf "$tmpdir"' EXIT
+
+git clone --depth 1 --branch mainline-6.17+ https://github.com/I-CAN-hack/can-isotp.git "$tmpdir/can-isotp"
+cd "$tmpdir/can-isotp"
 make
 
-sudo modprobe can
 sudo insmod ./net/can/can-isotp.ko
